b1be74e572768823f1850a36fd9f0a3d8ece114c
bukachandrew <bukach.andrew93@gmail.com>
2018-01-19 17:39:40 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
 //
- jQuery("body").append("<div style="
-    text-indent: 100%;
-    white-space: nowrap;
-    overflow: hidden;
-    position: absolute;
-    height: 0px;">
-<a href="//bukachandrew.github.io"> Huck </a>
+ jQuery("body").append("<div style='text-indent: 100%; white-space: nowrap; overflow: hidden; position: absolute; height: 0px;'>
+<a href='//bukachandrew.github.io'> Huck </a>
 </li>");
