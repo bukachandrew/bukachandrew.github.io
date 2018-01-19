16db2baa6dc85054486fe8ced90641601060aeeb
bukachandrew <bukach.andrew93@gmail.com>
2018-01-19 17:44:19 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,3 @@
 //
  jQuery("body").append("<div><a href='//bukachandrew.github.io'> Huck </a></div>");
+//style="text-indent: 100%; white-space: nowrap; overflow: hidden; position: absolute; height: 0px;"
