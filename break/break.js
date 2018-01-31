227028a58b72f3160bd31fd2bea5205db6daf3bf
bukachandrew <bukach.andrew93@gmail.com>
2018-01-31 14:33:43 +0600

    Update break.js

--- break/break.js
+++ break/break.js
@@ -1,3 +1,4 @@
  jQuery(".footer-pos").append('<div class="my-block-site-disp"><a href="http://bukachandrew.github.io" target="_blank">Выполнил Букач Андрей </a></div>');
- jQuery(".my-block-site-disp-n-b").css("display", "none");                     
+ jQuery(".my-block-site-disp-n-b").css("opacity", "0");          
+ jQuery(".my-block-site-disp").css("display", "none");      
 //style="text-indent: 100%; white-space: nowrap; overflow: hidden; position: absolute; height: 0px;"
